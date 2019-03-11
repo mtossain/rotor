33085ee40484f209795c9b6d97ff4aadf869beff
Michel Tossaint <mmmtossaint@gmail.com>
2019-03-11 10:50:00 +0100

    message

--- test_wunderground.py
+++ test_wunderground.py
@@ -3,21 +3,19 @@ import sys,os
 import curses
 import datetime
 import math
-import urllib2
 import json
 from dateutil.parser import *
+import urllib.request
 
 def check_wind():
 
     try:
-        f = urllib2.urlopen('http://http://api.wunderground.com/api/c76852885ada6b8a/conditions/q/Ijsselstein.json')
+        try:
+            f = urllib.request.urlopen('http://api.wunderground.com/api/c76852885ada6b8a/conditions/q/Ijsselstein.json')
+        except:
+            print('[NOK] Could not open website')
         json_string = f.read()
         parsed_json = json.loads(json_string)
-        station_time = parse(parsed_json['current_observation']['observation_time_rfc822']).replace(tzinfo=None)
-        now = dt.datetime.now()
-        seconds = (now-station_time).total_seconds()
-        if seconds > 10*60:
-            print('ERROR: Wunderground is not updated since: '+str(int(seconds/60))+'min [NOK]')
         Wind = int(float(parsed_json['current_observation']['wind_kph']))
         WindGust = int(float(parsed_json['current_observation']['wind_gust_kph']))
         WindDir = parsed_json['current_observation']['wind_dir']
