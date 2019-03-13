ccce010ae2f3665aca1c7e31552a2b3c8c01c777
Michel Tossaint <mmmtossaint@gmail.com>
2019-03-13 22:01:51 +0100

    adfa

--- test_process.py
+++ test_process.py
@@ -9,22 +9,24 @@ def read_az(azimuth,bias):
         print(azimuth.value)
 
 def read_el(elevation,bias):
-    false_reading,angle = read_el_ang()
-    if not false_reading:
-        elevation.value = angle - bias
-        print(elevation.value)
+    while (1):
+        false_reading,angle = read_el_ang()
+        if not false_reading:
+            elevation.value = angle - bias
+            print(elevation.value)
+        time.sleep(1)
 
 if __name__ == '__main__':
 
     azimuth = Value('d', 0.0) # shared between processes
     elevation = Value('d', 0.0) # shared between processes
 
-    p1 = Process(target=read_az, args=(azimuth,55))
-    p2 = Process(target=read_el, args=(elevation,55))
+    #p1 = Process(target=read_az, args=(azimuth,55))
+    p2 = Process(target=read_el, args=(elevation,52))
 
     #p1.start()
     p2.start()
 
     while(1):
-        print('In main')
+        print('In main: ',elevation.value)
         time.sleep(5)
