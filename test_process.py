00f00c6209cf746b3cc7f278fb2ccb00d9883614
Michel Tossaint <mmmtossaint@gmail.com>
2019-03-13 21:52:39 +0100

    test multiprocessing

--- test_process.py
+++ test_process.py
@@ -1,14 +1,28 @@
+from multiprocessing import Process, Value, Array
 
-def read_sensor():
+def read_az(azimuth,bias):
+    false_reading,angle = read_az_ang()
+    if not false_reading:
+        azimuth.value = (convert_az_reading(angle) - bias)%360
+        print(azimuth.value)
 
-    global conf,stat
+def read_el(elevation,bias):
+    false_reading,angle = read_el_ang()
+    if not false_reading:
+        elevation.value = angle - bias
+        print(elevation.value)
 
-    if az_sense_active:
-        state.az_false_reading,angle = read_az_ang() # Read azimuth sensor output
-        if not state.az_false_reading:
-            state.az_rep = (convert_az_reading(angle) - conf.bias_az)%360 # Convert for AMS5048 readings
+if __name__ == '__main__':
 
-    if el_sense_active:
-        state.el_false_reading,angle = read_el_ang()
-        if not state.el_false_reading:
-            state.el_rep = angle - conf.bias_el # Read elevation sensor output
+    azimuth = Value('d', 0.0) # shared between processes
+    elevation = Value('d', 0.0) # shared between processes
+
+    p1 = Process(target=read_az, args=(elevation,55))
+    p2 = Process(target=read_el, args=(azimuth,bias_az))
+
+    p1.start()
+    #p2.start()
+
+    while(1):
+        print('In main')
+        time.sleep(5)
